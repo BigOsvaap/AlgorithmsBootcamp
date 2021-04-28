d8b5801bf9087f9eadff31b452a421a1b3bfdf89
osvaa <bigosvaap@gmail.com>
2021-04-28 12:37:08 -0500

    Added queue implementation

--- src/main/java/stacksandqueues/Queue.java
+++ src/main/java/stacksandqueues/Queue.java
@@ -1,4 +1,37 @@
 package stacksandqueues;
 
-public class Queue {
+import utils.Node;
+
+public class Queue<T> {
+
+    Node<T> first;
+    Node<T> last;
+
+    public Node<T> pop(T value){
+        if (!isEmpty()){
+            var temp = first;
+            first = first.next;
+            return temp;
+        }
+        return null;
+    }
+
+    public void push(T value){
+        if (first == null){
+            first = new Node<>(value);
+            last = first;
+        }else{
+            last.next = new Node<>(value);
+            last = last.next;
+        }
+    }
+
+    public Node<T> peek(){
+        return first;
+    }
+
+    public boolean isEmpty(){
+        return first == null;
+    }
+
 }
